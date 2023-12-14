d1a82ebceed02dec393da29628ee0b97818e59a6
ManarAbdelhalimMohamed <145897768+ManarAbdelhalimMohahmed@users.noreply.github.com>
2023-12-14 08:55:31 +0200

    printmsg.c

--- printmsg.c
+++ printmsg.c
@@ -1,28 +1,12 @@
 #include "shell.h"
-
 /**
- * print_en - Print the environment.
- *
- * Return: void
- */
-void print_en(void)
+* manar_print - print
+*
+* @myText: int parameter
+*
+* Return: void
+*/
+void manar_print(const char *myText)
 {
-    int i = 0;
-
-    while (en[i] != NULL)
-    {
-        printf("%s\n", en[i]);
-        i++;
-    }
-}
-
-/**
- * message - Print message to stdout.
- * @msg: The message to be printed.
- *
- * Return: void
- */
-void message(const char *msg)
-{
-    printf("%s", msg);
+write(STDOUT_FILENO, myText, strlen(myText));
 }
